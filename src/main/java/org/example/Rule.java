f33e4d4399bbf86b061df6e0e8b515ae053ca83c
Gurbanov Tamerlan <t64566t@gmail.com>
2022-11-06 19:39:15 +0200

    Initial commit

---
+++ src/main/java/org/example/Rule.java
@@ -0,0 +1,15 @@
+package nau.lab6;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rule {
+    String rule;
+    List<Integer> listSingId;
+}
---
+++ src/main/java/org/example/Sign.java
@@ -0,0 +1,13 @@
+package nau.lab6;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sign {
+    Integer id;
+    String sign;
+}
